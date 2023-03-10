c40dff4ca7e9ebb170b50d234d4e8bbcb68a995a
David P. Sugar <david@thesugar.de>
2023-03-10 13:50:01 +0100

    updated

--- build.sh
+++ build.sh
@@ -8,13 +8,16 @@ zig build-obj \
     -target thumb-freestanding-eabihf \
     -mcpu=cortex_m4 \
     -lc \
-    --pkg-begin fido libs/fido2/src/main.zig \
-        --pkg-begin zbor libs/fido2/libs/zbor/src/main.zig \
-        --pkg-end \
-    --pkg-end \
+    --mod zbor::libs/fido2/libs/zbor/src/main.zig \
+    --mod fido:zbor:libs/fido2/src/main.zig \
+    --deps fido \
     -freference-trace \
     -OReleaseSmall \
     src/main.zig
+    #--pkg-begin fido libs/fido2/src/main.zig \
+    #    --pkg-begin zbor libs/fido2/libs/zbor/src/main.zig \
+    #    --pkg-end \
+    #--pkg-end \
 
 # Fetch dependencies
 make BOARD=nrf52840_mdk_dongle get-deps
